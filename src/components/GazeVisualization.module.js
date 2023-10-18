5a98de9edeb985ce5579bf9fb3eec074959e4ba4
seo <deergirl11@gmail.com>
2023-10-19 02:38:38 +0900

    add backgroundImg with opacity & draw line between fixations

--- src/components/GazeVisualization.module.js
+++ src/components/GazeVisualization.module.js
@@ -1,20 +1,34 @@
 import React, { Component } from "react";
 import fixationData from "../data/fixation_data.json";
+import GenerateImgUrl from "./GenerateImgUrl.module";
 
 class GazeVisualization extends Component {
   constructor(props) {
     super(props);
     this.state = {
       currentPage: 0,
+      previousCenter: null,
+      imgUrls: [],
     };
     this.canvasRefs = fixationData.map(() => React.createRef());
     this.animationFrameId = null;
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     console.log(fixationData);
-    this.animationFrameId = requestAnimationFrame(() => {
-      this.drawFixations();
+
+    // 이미지 URL 가져오기
+    const imgUrls = await Promise.all(
+      fixationData.map(async (page) => {
+        console.log("s_num, f_num", page.s_num, page.f_num);
+        return await GenerateImgUrl(page.s_num, page.f_num);
+      })
+    );
+
+    this.setState({ imgUrls }, () => {
+      this.animationFrameId = requestAnimationFrame(() => {
+        this.drawFixations();
+      });
     });
   }
 
@@ -24,7 +38,7 @@ class GazeVisualization extends Component {
     }
   }
 
-  drawFixations() {
+  drawFixations = async () => {
     const { currentPage } = this.state;
     const canvas = this.canvasRefs[currentPage].current; // 현재 페이지의 Canvas 참조 가져오기
 
@@ -50,6 +64,13 @@ class GazeVisualization extends Component {
       canvasHeight
     );
 
+    ctx.globalAlpha = 0.5;
+    const img = new Image();
+    img.src = this.state.imgUrls[currentPage];
+    img.onload = function () {
+      ctx.drawImage(img, 0, 0, canvasWidth, canvasHeight);
+    };
+
     // canvas.style.border = "1px solid #000";
 
     const animateFixations = (fixationIndex) => {
@@ -58,11 +79,14 @@ class GazeVisualization extends Component {
       if (fixationIndex >= fixations.length) {
         /**현재 페이지의 모든 fixations를 그렸다면 다음 페이지로 넘어가기 */
         if (currentPage < fixationData.length - 1) {
-          this.setState({ currentPage: currentPage + 1 }, () => {
-            this.animationFrameId = requestAnimationFrame(() => {
-              this.drawFixations();
-            });
-          });
+          this.setState(
+            { currentPage: currentPage + 1, previousCenter: null },
+            () => {
+              this.animationFrameId = requestAnimationFrame(() => {
+                this.drawFixations();
+              });
+            }
+          );
         }
         return;
       }
@@ -81,6 +105,14 @@ class GazeVisualization extends Component {
 
       const drawStep = () => {
         if (step >= steps) {
+          if (this.state.previousCenter) {
+            ctx.globalCompositeOperation = "source-over";
+            ctx.beginPath();
+            ctx.moveTo(
+              this.state.previousCenter.x,
+              this.state.previousCenter.y
+            );
+          }
           setTimeout(() => {
             animateFixations(fixationIndex + 1);
           }, interval);
@@ -91,13 +123,29 @@ class GazeVisualization extends Component {
 
         const x = (cx / 1080) * canvasWidth;
         const y = (cy / 2195) * canvasHeight;
+        const radius = r * progress * scaleFactor;
+
+        // ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        /**이전 fixation과 현재 fixation을 연결하는 선 그리기 */
+        if (this.state.previousCenter) {
+          ctx.globalCompositeOperation = "source-over";
+          ctx.beginPath();
+          ctx.moveTo(this.state.previousCenter.x, this.state.previousCenter.y);
+          ctx.lineTo(x, y);
+          ctx.strokeStyle = "#d11507";
+          ctx.stroke();
+        }
 
         /**원 그리기 */
+        ctx.globalAlpha = 0.5; // 투명도 50%
         ctx.globalCompositeOperation = "source-over";
         ctx.beginPath();
-        ctx.arc(x, y, r * progress * scaleFactor, 0, 2 * Math.PI);
-        ctx.fillStyle = "blue";
+        ctx.arc(x, y, radius, 0, 2 * Math.PI);
+        ctx.fillStyle = "#d11507";
         ctx.fill();
+        ctx.strokeStyle = "#d11507";
+        ctx.stroke();
 
         step++;
 
@@ -105,47 +153,21 @@ class GazeVisualization extends Component {
       };
 
       drawStep();
+
+      this.setState({
+        previousCenter: {
+          x: (cx / 1080) * canvasWidth,
+          y: (cy / 2195) * canvasHeight,
+        },
+      });
     };
 
     animateFixations(0);
-
-    //   const animate = () => {
-    //     const now = Date.now();
-    //     const elapsed = now - st;
-    //     const progress = Math.min(1, elapsed / duration);
-
-    //     console.log("Animating frame for fixation index", fixationIndex);
-    //     console.log("Now:", now);
-    //     console.log("Elapsed:", elapsed);
-    //     console.log("Progress:", progress);
-
-    //     /**화면 크기에 맞게 좌표 변환 */
-    //     const x = (cx / 1080) * canvasWidth;
-    //     const y = (cy / 2195) * canvasHeight;
-
-    //     /**원 그리기 */
-    //     ctx.globalCompositeOperation = "source-over";
-    //     ctx.beginPath();
-    //     ctx.arc(x, y, r * progress * scaleFactor, 0, 2 * Math.PI);
-    //     ctx.fillStyle = "blue";
-    //     ctx.fill();
-
-    //     if (progress < 1) {
-    //       requestAnimationFrame(animate);
-    //     } else {
-    //       setTimeout(() => {
-    //         animateFixations(fixationIndex + 1);
-    //       }, interval);
-    //     }
-    //   };
-
-    //   requestAnimationFrame(animate);
-    // };
-
-    // animateFixations(0);
-  }
+  };
 
   render() {
+    const { imgUrls } = this.state;
+
     return (
       <div style={{ display: "flex", overflowX: "scroll" }}>
         {fixationData.map((pageData, index) => (
@@ -154,7 +176,12 @@ class GazeVisualization extends Component {
             ref={this.canvasRefs[index]}
             width={1080}
             height={2195}
-            style={{ border: "1px solid #000", margin: "10px" }}
+            style={{
+              border: "1px solid #000",
+              margin: "10px",
+              // backgroundImage: `url(${imgUrls[index]})`,
+              // backgroundSize: "cover",
+            }}
           ></canvas>
         ))}
       </div>

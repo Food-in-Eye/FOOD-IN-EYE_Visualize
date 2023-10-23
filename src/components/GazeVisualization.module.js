2cf7b8a760bc00d5ba3908f84c2346a9bc3c23be
seo <deergirl11@gmail.com>
2023-10-24 01:43:08 +0900

    display canvases > display while switching to only one canvas

--- src/components/GazeVisualization.module.js
+++ src/components/GazeVisualization.module.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-// import fixationData from "../data/fixation_data.json";
 import visualizationData from "../data/visualization_data.json";
 import GenerateImgUrl from "./GenerateImgUrl.module";
+import Style from "../css/style.module.css";
 
 class GazeVisualization extends Component {
   constructor(props) {
@@ -10,25 +10,23 @@ class GazeVisualization extends Component {
       currentPage: 0,
       previousCenter: null,
       imgUrls: [],
+      fixationsIndex: 0,
     };
-    this.canvasRefs = visualizationData.map(() => React.createRef());
+    this.canvasRef = React.createRef();
     this.animationFrameId = null;
   }
 
   async componentDidMount() {
-    console.log(visualizationData);
-
-    // 이미지 URL 가져오기
+    // Load image URLs
     const imgUrls = await Promise.all(
       visualizationData.map(async (page) => {
-        console.log("s_num, f_num", page.s_num, page.f_num);
         return await GenerateImgUrl(page.s_num, page.f_num);
       })
     );
 
     this.setState({ imgUrls }, () => {
       this.animationFrameId = requestAnimationFrame(() => {
-        this.drawFixations();
+        this.drawCanvas();
       });
     });
   }
@@ -39,174 +37,147 @@ class GazeVisualization extends Component {
     }
   }
 
-  drawFixations = async () => {
-    const { currentPage } = this.state;
-    const canvas = this.canvasRefs[currentPage].current; // 현재 페이지의 Canvas 참조 가져오기
+  drawCanvas = () => {
+    const { currentPage, fixationsIndex } = this.state;
+    const canvas = this.canvasRef.current;
 
-    /**canvas가 null일때 아직 랜더링되지 않았으므로 재시도한다. */
     if (!canvas) {
       this.animationFrameId = requestAnimationFrame(() => {
-        this.drawFixations();
+        this.drawCanvas();
       });
       return;
     }
 
     const ctx = canvas.getContext("2d");
     const { fixations } = visualizationData[currentPage];
-
     const screenAspectRatio = 1080 / 2195;
     const canvasWidth = canvas.width;
     const canvasHeight = canvasWidth / screenAspectRatio;
 
-    console.log(
-      "canvas, canvasWidth, canvasHeight",
-      canvas,
-      canvasWidth,
-      canvasHeight
-    );
-
-    ctx.globalAlpha = 0.5;
-    const img = new Image();
-    img.src = this.state.imgUrls[currentPage];
-    img.onload = function () {
-      ctx.drawImage(img, 0, 0, canvasWidth, canvasHeight);
-    };
-
-    // canvas.style.border = "1px solid #000";
-
-    const animateFixations = (fixationIndex) => {
-      console.log("fixations", fixations);
-
-      if (fixationIndex >= fixations.length) {
-        /**현재 페이지의 모든 fixations를 그렸다면 다음 페이지로 넘어가기 */
-        if (currentPage < visualizationData.length - 1) {
-          this.setState(
-            { currentPage: currentPage + 1, previousCenter: null },
-            () => {
-              this.animationFrameId = requestAnimationFrame(() => {
-                this.drawFixations();
-              });
-            }
-          );
+    ctx.globalAlpha = 1;
+    if (fixationsIndex === 0) {
+      const img = new Image();
+      img.src = this.state.imgUrls[currentPage];
+      img.onload = () => {
+        ctx.drawImage(img, 0, 0, canvasWidth, canvasHeight);
+        if (fixations.length > 0) {
+          this.animationFrameId = requestAnimationFrame(() => {
+            this.drawFixations(fixations, ctx, canvasWidth, canvasHeight);
+          });
+        } else {
+          console.log("fixations가 없을 때1");
+          setTimeout(this.moveToNextCanvas, 2000);
         }
-        return;
+      };
+    } else {
+      if (fixations.length > 0) {
+        this.drawFixations(fixations, ctx, canvasWidth, canvasHeight);
+      } else {
+        console.log("fixations가 없을 때2");
+        setTimeout(this.moveToNextCanvas, 2000);
       }
+    }
+  };
 
-      const fixation = fixations[fixationIndex];
-      // const { cx, cy, st, et, r } = fixation;
-      const { x, y, d } = fixation;
-      // const duration = et - st;
+  drawFixations = (fixations, ctx, canvasWidth, canvasHeight) => {
+    const { fixationsIndex } = this.state;
+    if (fixationsIndex >= fixations.length) {
+      console.log("drawFixations");
+      setTimeout(this.moveToNextCanvas, 3000);
+      return;
+    }
 
-      const interval = 100; // 실제 속도
+    const fixation = fixations[fixationsIndex];
+    const { x, y, d } = fixation;
+    const interval = 100;
+    // const interval = 1000;
+    const steps = parseInt(d / 100);
+    const stepDuration = d / steps;
+    let step = 0;
+    let currentRadius = 0;
 
-      // const scaleFactor = 2;
-      const steps = parseInt(d / 100);
-      const stepDuration = d / steps;
+    const drawStep = () => {
+      if (step >= steps) {
+        if (fixationsIndex > 0) {
+          const prevFixation = fixations[fixationsIndex - 1];
+          const prevX = (prevFixation.x / 1080) * canvasWidth;
+          const prevY = (prevFixation.y / 2195) * canvasHeight;
+          const newX = (x / 1080) * canvasWidth;
+          const newY = (y / 2195) * canvasHeight;
 
-      let step = 0;
-      let currentRadius = 0;
-
-      const drawStep = () => {
-        if (step >= steps) {
-          if (this.state.previousCenter) {
-            ctx.globalCompositeOperation = "source-over";
-            ctx.beginPath();
-            ctx.moveTo(
-              this.state.previousCenter.x,
-              this.state.previousCenter.y
-            );
-          }
-          setTimeout(() => {
-            animateFixations(fixationIndex + 1);
-          }, interval);
-          return;
-        }
-
-        const progress = step / steps;
-
-        const newX = (x / 1080) * canvasWidth;
-        const newY = (y / 2195) * canvasHeight;
-        const radius = 10 * step * progress;
-        // const radius = r * progress * scaleFactor;
-
-        // ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        /**이전 fixation과 현재 fixation을 연결하는 선 그리기 */
-        if (this.state.previousCenter) {
           ctx.globalCompositeOperation = "source-over";
           ctx.beginPath();
-          ctx.moveTo(this.state.previousCenter.x, this.state.previousCenter.y);
+          ctx.moveTo(prevX, prevY);
           ctx.lineTo(newX, newY);
           ctx.strokeStyle = "#d11507";
           ctx.stroke();
         }
 
-        /**원 그리기 */
-        ctx.globalAlpha = 0.5; // 투명도 50%
-        ctx.globalCompositeOperation = "source-over";
-        // ctx.beginPath();
-        if (step === 0) {
-          ctx.beginPath();
-          ctx.arc(newX, newY, radius, 0, 2 * Math.PI);
-          ctx.fillStyle = "#d11507";
-          ctx.fill();
-          // ctx.strokeStyle = "#d11507";
-          // ctx.stroke();
-        } else {
-          // ctx.clearRect(
-          //   newX - currentRadius,
-          //   newY - currentRadius,
-          //   currentRadius * 2,
-          //   currentRadius * 2
-          // );
-          ctx.beginPath();
-          ctx.arc(newX, newY, currentRadius, 0, 2 * Math.PI);
-          ctx.arc(newX, newY, currentRadius - 5, 0, 2 * Math.PI);
-          ctx.fillStyle = "rgba(209,21,7)";
-          ctx.fill();
-          // ctx.strokeStyle = "#d11507";
-          // ctx.stroke();
-        }
-        // ctx.arc(newX, newY, radius, 0, 2 * Math.PI);
-        // ctx.fillStyle = "#d11507";
-        // ctx.fill();
-        // ctx.strokeStyle = "#d11507";
-        // ctx.stroke();
+        this.setState({ fixationsIndex: fixationsIndex + 1 }, () => {
+          setTimeout(this.drawCanvas, interval);
+        });
+        return;
+      }
 
-        step++;
-        currentRadius += 10;
+      const progress = step / steps;
+      const newX = (x / 1080) * canvasWidth;
+      const newY = (y / 2195) * canvasHeight;
+      const radius = 10 * step * progress;
 
-        setTimeout(drawStep, stepDuration);
-      };
+      if (step === 0) {
+        ctx.globalAlpha = 0.5;
+        ctx.beginPath();
+        ctx.arc(newX, newY, radius, 0, 2 * Math.PI);
+        ctx.fillStyle = "#d11507";
+        ctx.fill();
+      } else {
+        ctx.globalAlpha = 0.5;
+        ctx.beginPath();
+        ctx.arc(newX, newY, currentRadius, 0, 2 * Math.PI);
+        ctx.arc(newX, newY, currentRadius - 5, 0, 2 * Math.PI);
+        ctx.fillStyle = "rgba(209,21,7)";
+        ctx.fill();
+      }
 
-      drawStep();
+      step++;
+      currentRadius += 10;
 
-      this.setState({
-        previousCenter: {
-          x: (x / 1080) * canvasWidth,
-          y: (y / 2195) * canvasHeight,
-        },
-      });
+      setTimeout(drawStep, stepDuration);
     };
 
-    animateFixations(0);
+    drawStep();
+  };
+
+  moveToNextCanvas = () => {
+    const { currentPage } = this.state;
+    if (currentPage < visualizationData.length - 1) {
+      this.setState(
+        {
+          currentPage: currentPage + 1,
+          fixationsIndex: 0,
+          previousCenter: null,
+        },
+        () => {
+          this.animationFrameId = requestAnimationFrame(() => {
+            this.drawCanvas();
+          });
+        }
+      );
+    }
   };
 
   render() {
     return (
-      <div style={{ display: "flex", overflowX: "scroll" }}>
-        {visualizationData.map((pageData, index) => (
-          <canvas
-            key={index}
-            ref={this.canvasRefs[index]}
-            width={1080}
-            height={2195}
-            style={{
-              border: "1px solid #000",
-              margin: "10px",
-            }}
-          ></canvas>
-        ))}
+      <div className={Style.canvasDiv}>
+        <canvas
+          ref={this.canvasRef}
+          width={1080}
+          height={2195}
+          style={{
+            border: "1px solid #000",
+            margin: "10px",
+          }}
+        ></canvas>
       </div>
     );
   }
